bd1ea0679e1027a2d8ca408a88d70f81c26dbe59
takaogaw <color_me_pop@live.jp>
2017-05-27 18:02:47 +0900

    雛形はOK

--- tasks/template.js
+++ tasks/template.js
@@ -1,17 +1,14 @@
 import gulp from 'gulp';
 import gulpLoadPlugins from 'gulp-load-plugins';
 import pkg from '../package.json';
-import sitedata from '../sitedata.json';
+import sitedata from '../site_content.json';
 
 const $ = gulpLoadPlugins();
 const templatePath = pkg.path.srcTemplate;
 const dest = pkg.path.dest;
 const stylePath = pkg.path.srcStyle;
 
-
-gulp.task('template', () => {
-
-  console.log('template is excuted')
+gulp.task('template:static', () => {
 
   const stream = gulp.src([
     templatePath + '*.ejs',
@@ -27,3 +24,25 @@ gulp.task('template', () => {
 
   return stream;
 });
+
+gulp.task('template:generate', () => {
+
+  sitedata.pages.feature.forEach((page) => {
+
+    const stream = gulp.src([
+      templatePath + 'feature/_template.ejs'
+    ])
+    .pipe($.plumber())
+    .pipe($.rename(page.id))
+    .pipe($.ejs({
+      page: page,
+      original: sitedata
+    }, {}, {
+      ext: '.html'
+    }))
+    .pipe(gulp.dest(dest + 'html/feature'));
+
+  });
+});
+
+gulp.task('template', ['template:static', 'template:generate']);
