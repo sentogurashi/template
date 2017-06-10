63573c8eaea79a254c572748f558fe79374197f9
takaogaw <color_me_pop@live.jp>
2017-06-10 18:11:43 +0900

    yaml追加忘れ

---
+++ tasks/yaml.js
@@ -0,0 +1,12 @@
+import gulp from 'gulp';
+import gulpLoadPlugins from 'gulp-load-plugins';
+
+const $ = gulpLoadPlugins();
+
+gulp.task('yaml', () => {
+  return gulp.src('./site_content.yml')
+    .pipe($.yaml({
+      space: 2
+    }))
+    .pipe(gulp.dest('./'));
+});
