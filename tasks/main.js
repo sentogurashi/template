1ccefcf7dfb53317b367e4c19faac153a432fb0b
takaogaw <color_me_pop@live.jp>
2017-06-14 14:42:15 +0900

    deploy時にかならずビルド、プロフィール調整

--- tasks/main.js
+++ tasks/main.js
@@ -1,10 +1,17 @@
 import gulp from 'gulp';
 import runSequence from 'run-sequence';
 
-gulp.task('default', (callback) => {
+gulp.task('generate', (callback) => {
   return runSequence(
     'clean',
     ['style', 'script', 'template'],
+    callback
+  );
+});
+
+gulp.task('default', (callback) => {
+  return runSequence(
+    'generate',
     'watch',
     callback
   );
